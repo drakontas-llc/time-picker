750d85bae1630848cc5f180795f32e8deced3d66
Nick <vurean@outlook.com>
2019-05-06 16:08:07 +0300

    chore: push compiled version

--- es/Combobox.js
+++ es/Combobox.js
@@ -133,40 +133,41 @@ var Combobox =
           isAM = _this$props.isAM,
           onAmPmChange = _this$props.onAmPmChange;
         var value = (propValue || defaultOpenValue).clone();
+        var newValue = value;
 
         if (type === 'hour') {
           if (use12Hours) {
             if (isAM) {
-              value.hour(+itemValue % 12);
+              newValue = value.hour(+itemValue % 12);
             } else {
-              value.hour((+itemValue % 12) + 12);
+              newValue = value.hour((+itemValue % 12) + 12);
             }
           } else {
-            value.hour(+itemValue);
+            newValue = value.hour(+itemValue);
           }
         } else if (type === 'minute') {
-          value.minute(+itemValue);
+          newValue = value.minute(+itemValue);
         } else if (type === 'ampm') {
           var ampm = itemValue.toUpperCase();
 
           if (use12Hours) {
             if (ampm === 'PM' && value.hour() < 12) {
-              value.hour((value.hour() % 12) + 12);
+              newValue = value.hour((value.hour() % 12) + 12);
             }
 
             if (ampm === 'AM') {
               if (value.hour() >= 12) {
-                value.hour(value.hour() - 12);
+                newValue = value.hour(value.hour() - 12);
               }
             }
           }
 
           onAmPmChange(ampm);
         } else {
-          value.second(+itemValue);
+          newValue = value.second(+itemValue);
         }
 
-        onChange(value);
+        onChange(newValue);
       });
 
       _defineProperty(_assertThisInitialized(_this), 'onEnterSelectPanel', function(range) {
